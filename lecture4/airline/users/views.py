7661b6c3f1366e083a53e1353d1c1769c3a9bdcc
ThiagoMMonteiro <thiago.monteiro2608@gmail.com>
2021-04-08 20:20:14 -0300

    Fixes in views.py

--- lecture4/airline/users/views.py
+++ lecture4/airline/users/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+
 # Create your views here.
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     return render(request, "users/user.html")
+
 
 def login_view(request):
     if request.method == "POST":
@@ -22,6 +24,7 @@ def login_view(request):
                 "message": "Invalid Credentials."
             })
     return render(request, "users/login.html")
+
 
 def logout_view(request):
     logout(request)
